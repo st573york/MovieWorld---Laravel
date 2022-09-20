a1239ba9fed97d376c34614cfa5df754a324189a
Stelios Tsallas <steliostsallas@gmail.com>
2022-09-20 18:20:07 +0300

    removed unused stuff

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -8,7 +8,6 @@ use App\Review;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 
 class MoviesController extends Controller
